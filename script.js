d754148a8708545711589a38952341c92fc3d9b3
OleanjiKingCode <holamilean56@gmail.com>
2022-04-07 10:56:43 +0100

    sample commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $("#comments .owl-carousel").owlCarousel({
+            items:1,
+            loop:true,
+            autoplay:true,
+            autoplayHoverPause:true,
+            dots:false
+    
+        });
+    
+    
+    });
